3c0899dd182912dc1a51e65f0b5dda1d85176da0
Lauren Eggers <neyabenz@gmail.com>
2019-09-04 16:10:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const cards = [
+    {
+      front: 'The "First Computer Programmer"',
+      back: 'Ada Lovelace',
+      flipped: false,
+    },
+    {
+      front: 'Invented the "Clarke Calculator"',
+      back: 'Edith Clarke',
+      flipped: false,
+
+    },
+    {
+      front: 'Famous World War II Enigma code breaker',
+      back: 'Alan Turing',
+      flipped: false,
+    },
+    {
+      front: 'Created satellite orbit analyzation software for NASA',
+      back: 'Dr. Evelyn Boyd Granville',
+      flipped: false,
+    },
+  ];
+
+  new Vue({
+    el: '#flashcard-app',
+    data: {
+      cards: cards,
+    },
+    methods: {
+      //flipCard
+      flipCard: function(card, index) {
+        this.cards[index].flipped = !this.cards[index].flipped;
+      },
+      deleteCard: function(index) {
+        cards.splice(index, 1);
+      }
+    }
+  });
